75c3356c7876b1efba563161d8b5250abd80c5ad
Diwarrss <diegovargas@unisangil.edu.co>
2019-09-12 22:55:48 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -38135,8 +38135,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\coreuiTemplate\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\coreuiTemplate\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! c:\laragon\www\centuriaU\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! c:\laragon\www\centuriaU\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
