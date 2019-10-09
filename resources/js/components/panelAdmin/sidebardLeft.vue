86643b6e8a3375a05ff1d296216405012677e894
Diwarrss <diegovargas@unisangil.edu.co>
2019-10-09 15:54:49 -0500

    Update sidebardLeft.vue
    antes de implementar menu por Rol

--- resources/js/components/panelAdmin/sidebardLeft.vue
+++ resources/js/components/panelAdmin/sidebardLeft.vue
@@ -1,70 +1,216 @@
 <template>
-  <div class="sidebar">
-    <nav class="sidebar-nav">
+  <div class="sidebar" v-if="infoUserAuth.length">
+    <div class="sidebar-nav">
       <ul class="nav">
-        <li class="nav-item">
-          <router-link class="nav-link" to="/">
-            <i class="nav-icon icon-speedometer"></i> Escritorio
-            <span class="badge badge-primary">Ver</span>
-          </router-link>
-        </li>
-        <li class="nav-title text-center text-primary">
-          <i class="nav-icon icon-home"></i> Menú
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/ingresos">
-            <i class="nav-icon icon-user-following"></i> Ingresos
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/personas">
-            <i class="nav-icon icon-people"></i> Personas
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/computadores">
-            <i class="nav-icon icon-screen-desktop"></i> Computadores
-          </router-link>
-        </li>
-        <li class="nav-item nav-dropdown">
-          <a class="nav-link nav-dropdown-toggle" href="#">
-            <i class="nav-icon icon-chart"></i> Reportes
-          </a>
-          <ul class="nav-dropdown-items">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/total">
-                <i class="nav-icon icon-puzzle"></i> Totales
-              </router-link>
-            </li>
-          </ul>
-        </li>
-        <li class="nav-title text-center text-primary">
-          <i class="nav-icon icon-equalizer"></i> Configuración
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/usuarios">
-            <i class="nav-icon fas fa-users-cog"></i> Usuarios
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/universidad">
-            <i class="nav-icon fas fa-university"></i> Universidad
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/perfil">
-            <i class="nav-icon icon-user"></i> Perfil
-          </router-link>
-        </li>
+        <!-- SUPERADMIN -->
+        <section v-if="infoUserAuth[0].roles_id == 1">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">
+              <i class="nav-icon icon-speedometer"></i> Escritorio
+              <span class="badge badge-primary">Ver</span>
+            </router-link>
+          </li>
+          <li class="nav-title text-center text-primary">
+            <i class="nav-icon icon-home"></i> Menú
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/ingresos">
+              <i class="nav-icon icon-user-following"></i> Ingresos
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/personas">
+              <i class="nav-icon icon-people"></i> Personas
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/computadores">
+              <i class="nav-icon icon-screen-desktop"></i> Computadores
+            </router-link>
+          </li>
+          <li class="nav-item nav-dropdown">
+            <a class="nav-link nav-dropdown-toggle" href="#">
+              <i class="nav-icon icon-chart"></i> Reportes
+            </a>
+            <ul class="nav-dropdown-items">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/total">
+                  <i class="nav-icon icon-puzzle"></i> Totales
+                </router-link>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-title text-center text-primary">
+            <i class="nav-icon icon-equalizer"></i> Configuración
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/usuarios">
+              <i class="nav-icon fas fa-users-cog"></i> Usuarios
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/universidad">
+              <i class="nav-icon fas fa-university"></i> Universidad
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/perfil">
+              <i class="nav-icon icon-user"></i> Perfil
+            </router-link>
+          </li>
+        </section>
+        <!-- ADMIN -->
+        <section v-else-if="infoUserAuth[0].roles_id == 2">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">
+              <i class="nav-icon icon-speedometer"></i> Escritorio
+              <span class="badge badge-primary">Ver</span>
+            </router-link>
+          </li>
+          <li class="nav-title text-center text-primary">
+            <i class="nav-icon icon-home"></i> Menú
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/ingresos">
+              <i class="nav-icon icon-user-following"></i> Ingresos
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/personas">
+              <i class="nav-icon icon-people"></i> Personas
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/computadores">
+              <i class="nav-icon icon-screen-desktop"></i> Computadores
+            </router-link>
+          </li>
+          <li class="nav-item nav-dropdown">
+            <a class="nav-link nav-dropdown-toggle" href="#">
+              <i class="nav-icon icon-chart"></i> Reportes
+            </a>
+            <ul class="nav-dropdown-items">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/total">
+                  <i class="nav-icon icon-puzzle"></i> Totales
+                </router-link>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-title text-center text-primary">
+            <i class="nav-icon icon-equalizer"></i> Configuración
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/usuarios">
+              <i class="nav-icon fas fa-users-cog"></i> Usuarios
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/perfil">
+              <i class="nav-icon icon-user"></i> Perfil
+            </router-link>
+          </li>
+        </section>
+        <!-- INGRESO O REGISTROS -->
+        <section v-else-if="infoUserAuth[0].roles_id == 3">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">
+              <i class="nav-icon icon-speedometer"></i> Escritorio
+              <span class="badge badge-primary">Ver</span>
+            </router-link>
+          </li>
+          <li class="nav-title text-center text-primary">
+            <i class="nav-icon icon-home"></i> Menú
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/ingresos">
+              <i class="nav-icon icon-user-following"></i> Ingresos
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/personas">
+              <i class="nav-icon icon-people"></i> Personas
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/computadores">
+              <i class="nav-icon icon-screen-desktop"></i> Computadores
+            </router-link>
+          </li>
+          <li class="nav-item nav-dropdown">
+            <a class="nav-link nav-dropdown-toggle" href="#">
+              <i class="nav-icon icon-chart"></i> Reportes
+            </a>
+            <ul class="nav-dropdown-items">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/total">
+                  <i class="nav-icon icon-puzzle"></i> Totales
+                </router-link>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-title text-center text-primary">
+            <i class="nav-icon icon-equalizer"></i> Configuración
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/perfil">
+              <i class="nav-icon icon-user"></i> Perfil
+            </router-link>
+          </li>
+        </section>
+        <!-- CONSULTA -->
+        <section v-else>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">
+              <i class="nav-icon icon-speedometer"></i> Escritorio
+              <span class="badge badge-primary">Ver</span>
+            </router-link>
+          </li>
+          <li class="nav-title text-center text-primary">
+            <i class="nav-icon icon-home"></i> Menú
+          </li>
+
+          <li class="nav-item nav-dropdown">
+            <a class="nav-link nav-dropdown-toggle" href="#">
+              <i class="nav-icon icon-chart"></i> Reportes
+            </a>
+            <ul class="nav-dropdown-items">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/total">
+                  <i class="nav-icon icon-puzzle"></i> Totales
+                </router-link>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-title text-center text-primary">
+            <i class="nav-icon icon-equalizer"></i> Configuración
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/perfil">
+              <i class="nav-icon icon-user"></i> Perfil
+            </router-link>
+          </li>
+        </section>
       </ul>
-    </nav>
+    </div>
     <button class="sidebar-minimizer brand-minimizer" type="button"></button>
   </div>
 </template>
 <script>
+//importamos para usar el mapState, mapActions
+import { mapState, mapActions } from "vuex";
 export default {
   data() {
     return {};
+  },
+  computed: {
+    ...mapState(["infoUserAuth"])
+  },
+  methods: {
+    ...mapActions(["getUserAuth"])
+  },
+  mounted() {
+    this.getUserAuth();
   }
 };
 </script>
