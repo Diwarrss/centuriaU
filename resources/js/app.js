a597a652efc8db7a8a160d196c66e5fd59988065
Diwarrss <diegovargas@unisangil.edu.co>
2019-10-18 18:09:38 -0500

    Cambiar logica de buscar persona y guardar en mi bd

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,3 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
 /* Trae Axios */
 require("./bootstrap");
 
@@ -32,17 +27,6 @@ import Datetime from "vue-datetime";
 Vue.use(Datetime);
 // You need a specific loader for CSS files
 import "vue-datetime/dist/vue-datetime.css";
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component(
     "navbarheader",
@@ -113,16 +97,8 @@ const Usuarios = require("./components/allMenusInfo/Usuarios.vue").default;
 const Universidad = require("./components/allMenusInfo/Universidad.vue")
     .default;
 const Perfil = require("./components/allMenusInfo/Perfil.vue").default;
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+
 // 2. Define some routes
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// `Vue.extend()`, or just a component options object.
-// We'll talk about nested routes later.
 const routes = [{
         path: "*",
         component: Error404
@@ -162,10 +138,8 @@ const routes = [{
 ];
 
 // 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = new VueRouter({
-    //mode: 'history',//quitando el hash # que viene por defecto con vue-router
+    //mode: 'history', //quitando el hash # que viene por defecto con vue-router
     routes, // short for `routes: routes`
     base: "/admin"
 });
